d31e2ba17c330474206935147ff43b01f8320cb8
OleksandrMelnychuk <OleksandrMelnychuk@localhost>
2017-05-30 13:24:28 +0200

    rimozione source

--- src/main/java/util/ApplicationConfiguration.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class ApplicationConfiguration {
-	public static final int NUMBER_OF_TABLES = 24;
-	public static final String NEWLINE = "\n";
-	public static final String RESERVATION_FILE = "/reservations.xml";
-}
